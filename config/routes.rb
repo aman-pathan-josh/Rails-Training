8414f5a75f57fcf3833262a4f85dfb66ac452945
aman-pathan-josh <aman.pathan@joshsoftware.com>
2025-01-30 15:47:11 +0530

    Added devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
+  root to: "home#index"
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
